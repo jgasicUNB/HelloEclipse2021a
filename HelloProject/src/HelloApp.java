ae89cdfed62da76f0656c45a067add128a60b1f1
JaspeerGasic <jgasic@unb.ca>
2021-11-04 16:07:25 -0300

    version1

--- HelloProject/src/HelloApp.java
+++ HelloProject/src/HelloApp.java
@@ -3,7 +3,7 @@ public class HelloApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Jaspeer!");
+		System.out.println("Hello Jaspeer1!");
 	}
 
 }
